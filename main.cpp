2ea47d192f13669f669ce21ceaee456961c2ad1b
alain0077 <alain@ngc.is.ritsumei.ac.jp>
2022-12-09 01:13:23 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ int RAND()
 
     std::random_device rnd;
     std::mt19937 mt(rnd());
-    std::uniform_int_distribution<> randN(1, N - 1);  //生成する乱数の値の範囲
+    std::uniform_int_distribution<> randN(1, Define::N - 1);  //生成する乱数の値の範囲
     rand = randN(mt);
 
     return rand;
@@ -60,87 +60,11 @@ int RAND03()
     return rand;
 }
 
-void test()
-{
-    int i = 1023;
-    //int j = 0;
-
-    double times = 0;
-    double sum_err_m = 0;  // 演算誤差の合計
-    double percent_m = 0;  // エラー率の合計
-    double err_max = 0;
-    double err = 0;  double per = 0;
-
-    FILE* fp = fopen("test.csv", "w");
-    if (fp == NULL) {
-        cout << "miss" << endl;
-    }
-
-    for (int j = 1; j < i; j++) {
-
-        /*
-        * 実際の演算結果を求める
-        */
-        double x = (double)i / (double)N;  // xの値
-        double y = (double)j / (double)N;  // yの値
-        double ans = y / x;  // 除算
-        //sum_ans += ans;
-
-        //入力
-        SN p1(i, RAND(), 2);
-        SN p3(j, RAND(), 0);
-
-        /*
-        * JK-FF
-        * 真理値表
-        * J=0,K=0でQを保持
-        * J=0,K=1でQを0
-        * J=1,K=0でQを1
-        * J=1,K=1でQを反転
-        */
-        bitset<N> JK;
-        bool Q = false;
-
-        //calculateの出力
-        bool cal = false;
-
-        for (int k = 0; k < N; k++) {
-            if (!cal && !p3.get_out()[k]) JK[k] = Q;
-            if (!cal && p3.get_out()[k]) JK[k] = 0;
-            if (cal && !p3.get_out()[k]) JK[k] = 1;
-            if (cal && p3.get_out()[k]) JK[k] = !Q;
-
-            Q = JK[k];
-
-            cal = !JK[k] && p1.get_out()[k];
-        }
-        err = (1.0 - (double)JK.count() / (double)N);
-        err *= 2.0;
-        err -= ans;
-        fprintf(fp, "%d,", j);
-        fprintf(fp, "%f,", ans);
-        fprintf(fp, "%f,", abs(err)/1.0);
-        fprintf(fp, "%f,", ((double)JK.count() / (double)N));
-        fprintf(fp, "%f,", ((double)1.0 - (double)JK.count() / (double)N));
-        fprintf(fp, "%f\n", ((double)1.0 - (double)JK.count() / (double)N) * 2.0);
-    }
-
-    //err = ((double)1.0 - (double)JK.count() / (double)N) - ans;   // 誤差
-    //err_max = max(err_max, abs(err));
-    //sum_err_m += abs(err);        // 二乗誤差
-    //per = err / ans;              // エラー率
-    //percent_m += abs(per);        // 二乗エラー率
-}
-
 int main()
 {
-    /*/*/
-    test();
-    /*/
-    div();
-    /**/
-    
-    //mux(mode);
+    SN* sn1 = new SN(10,RAND(),3);
+
+    sn1->print_bs();
 
     return 0;
 }
