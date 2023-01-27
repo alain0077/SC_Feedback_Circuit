f185be349e7ee248db7fdcc5e3a7387ff782cdd8
aru <is0447fe.ed.ritsumei.ac.jp>
2023-01-28 04:49:13 +0900

    Update Min.cpp

--- Min.cpp
+++ Min.cpp
@@ -8,15 +8,15 @@ AND::AND() : Analysis()
 
 SN AND::operator() (SN left, SN right)
 {
-    auto sn = left.get_sn() & right.get_sn();
+    auto min = left.get_sn() & right.get_sn();
 
     double ans = left.get_ans() < right.get_ans() ? left.get_ans() : right.get_ans();
 
-    double val = (double)sn.count() / (double) Define::N;
+    double val = (double)min.count() / (double) Define::N;
 
     double scc = left.SCC(right);
 
     Update_Analysis(ans, val, scc);
 
-    return SN(ans, val, sn);
+    return SN(ans, val, min);
 }
